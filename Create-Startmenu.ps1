164b1769169285c5f1900a8245a216ce156a09e9
William Garrod <willgarrod07@gmail.com>
2014-11-14 14:03:17 +0000

    Fixed the indenting of both files

--- Create-Startmenu.ps1
+++ Create-Startmenu.ps1
@@ -1,13 +1,13 @@
 ﻿$Key_Path = Test-Path HKLM:\SOFTWARE\Deployment
 
-IF ($Key_Path = "True")
-    {Remove-Item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\*" -Recurse -Force
-     #New-Item -ItemType Directory -Path "C:\ProgramData\Microsoft\Windows\Start Menu"
-     $Key = (Get-ItemProperty -Path HKLM:\Software\Deployment\ -Name MDTRoles).MDTRoles
-     $Split_Key = $key -split ';'
+IF ($Key_Path = "True"){
+    Remove-Item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\*" -Recurse -Force
+    #New-Item -ItemType Directory -Path "C:\ProgramData\Microsoft\Windows\Start Menu"
+    $Key = (Get-ItemProperty -Path HKLM:\Software\Deployment\ -Name MDTRoles).MDTRoles
+    $Split_Key = $key -split ';'
 
-     For($i =1; $i -le $Split_key.count; $i++)
-    {$Share_Path = "\\wildfire\distribution\Startmenu\" + $Split_Key[$i - 1] + "\*"
-     Copy-Item -Path $Share_Path -Destination  "C:\ProgramData\Microsoft\Windows\Start Menu\Programs" -Recurse
+    For($i =1; $i -le $Split_key.count; $i++){
+    $Share_Path = "\\wildfire\distribution\Startmenu\" + $Split_Key[$i - 1] + "\*"
+    Copy-Item -Path $Share_Path -Destination  "C:\ProgramData\Microsoft\Windows\Start Menu\Programs" -Recurse
     }
     }
